f41906ed73852af0fa0166f19fcb26f9a9d458a6
Fake Name <fake.email@domain.com>
2017-04-30 04:10:01 -0400

    proccesslist/script

---
+++ listscpt.m
@@ -0,0 +1,23 @@
+clear; clc;
+
+% all vids that didnt error out
+gfid = fopen('completedlist.txt');
+goodvids = textscan(gfid,'%s','Delimiter','\n');
+goodvids = goodvids{1};
+goodvids = unique(goodvids);
+size(goodvids)
+
+% vids caught by error checking
+bfid = fopen('badlist.txt');
+badvids = textscan(bfid,'%s','Delimiter','\n');
+badvids = badvids{1};
+badvids = unique(badvids);
+size(badvids)
+
+% vids that didnt error - vids that didnt pass try/catch block
+% == all good vids to process
+processlist = setdiff(goodvids,badvids);
+
+fileP = fopen('processlist.txt','w');
+fprintf(fileP,'%s\n',processlist{:});
+fclose(fileP);
